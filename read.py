e333634cce60195967f8d9993f64670846f5d049
AlexisEidelman <alexis.eidelman.pro@gmail.com>
2015-03-20 14:22:14 +0100

    read_table1

---
+++ read.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 19 17:27:32 2015
+
+@author: Alexis Eidelman
+"""
+
+import pandas as pd
+import os
+
+path = 'D:\data\BSPP'
+main_file = os.path.join(path, '2015-043-Extraction LCL PAGNIEZ.csv')
+
+tab = pd.read_csv(main_file, sep=',', encoding='utf8')
+
+## quelques stats
+tab['Id_Intervention'].value_counts()
+tab[u'Id_Statut_Operationnel_Libelle_Statut_Operationnel'].value_counts()
+
+# recherche de variables redondantes
+tab.groupby(['Id_Intervention_Abrege_Motif', 'Code_Cri']).size()
+assert tab.groupby(['Id_Statut_Operationnel_Libelle_Statut_Operationnel'])['Abrege_Statut_Operationnel'].nunique() == 1
+# => on a une redondance, virer l'une des deux
+del tab['Abrege_Statut_Operationnel']
+## sujet: brainstorm
+
+# étudier les différences entre 'Id_Intervention_Abrege_Motif' et 'cri'
+# ce qui est établi et ce qui est constaté
+# => Demander à la BSPP si c'est grave de se tromper d'intervention, faire baisser ce taux en
+# regardant dans quels cas il se produit
+tab.groupby(['Id_Intervention_Abrege_Motif', 'Code_Cri']).size()
+
+## expliquer l'indisponibilité des véhicules
+# quel heure ont lieu les manques de personnels !
+tab[u'Id_Statut_Operationnel_Libelle_Statut_Operationnel'].value_counts()
+
+## guide des intéractions avec l'hopital
+hopital = [u'Transport hôpital', u"Arrivée hôpital", u"Quitte hôpital"]
+tab_hopital = tab[tab[u'Id_Statut_Operationnel_Libelle_Statut_Operationnel'].isin(hopital)]
+assert len(tab_hopital) == 33302  #
+tab_hopital['Id_Intervention'].value_counts()
+# un exemple : il y a plusieurs véhicules par intérventions
+# -> on peut suivre le véhicule et voir combien de temps il reste à l'hopital
+# une stat à améliorer ou bien il faut laisser les pomiers draguer les infirmières ?
+tab_hopital[tab_hopital['Id_Intervention'] == 8420079]
+
+
+## regarder les déplacements hors zone...
+
+# TODO: changer le nom des variables à rallonge
